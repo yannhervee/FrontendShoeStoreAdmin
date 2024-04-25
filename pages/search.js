a544b43ab0b13e95e62343e5663a9fbcb28fb7b8
animan alloua yannhervee <animan001@gannon.edu>
2024-04-24 20:08:46 -0400

    added import

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
+import Link from 'next/link';
 
 const SearchResultsPage = () => {
   const [products, setProducts] = useState([]);
